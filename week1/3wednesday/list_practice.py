ae8103efc2f8583efb1b6d682c2f12c3c6bcd3a8
kevinkusky <kevinkusky@gmail.com>
2021-01-02 23:05:10 -0500

    explore the list

---
+++ week1/3wednesday/list_practice.py
@@ -0,0 +1,57 @@
+# DO NOT EDIT - Starting with a simple lists of colors and numbers
+colors = ["blue", "Green", "PURPLE", "blue-green", "sky blue"]
+numbers = [2, 34, 8.5, -22.0, 33//4, 2**5]
+print ('COLORS', colors)
+print ('NUMBERS', numbers)
+
+# 1. Print the total number of colors (length of the list)
+
+print('Colors Length: ', len(colors))
+
+# 2. Print the first color
+
+print('First Color: ', colors[0].title())
+
+
+# 3. Print the second and third colors
+
+
+# def titleFunc(word):
+#   return word.title()
+
+
+# twoThree = list(map(titleFunc, colors[1:3])
+
+                
+# print('Color Two and Three: ', twoThree)
+print(colors[1:3])
+
+
+# 4. Print the last two colors
+
+print(colors[-2:])
+
+
+# 5. Print the smallest number in the numbers list
+
+print(min(numbers))
+
+
+# 6. Print the largest number in the numbers list
+
+print(max(numbers))
+
+
+# 7. Sort the numbers
+
+print(sorted(numbers))
+
+
+# UNCOMMENT WHEN YOU WORK ON #7
+# print ('SORTED NUMBERS', numbers)
+
+# 8. Sort the colors alphabetically ignoring case
+
+
+# UNCOMMENT WHEN YOU WORK ON #8
+print ('SORTED COLORS', sorted(colors, key=str.upper))
