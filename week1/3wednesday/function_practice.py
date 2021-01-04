53771208a58c0512d32c35be5e9a448c805758c1
kevinkusky <kevinkusky@gmail.com>
2021-01-03 22:39:54 -0500

    can nest and greeting

--- week1/3wednesday/function_practice.py
+++ week1/3wednesday/function_practice.py
@@ -39,3 +39,35 @@ print(get_indices([1, 5, 5, 2, 7], 5))
 
 print(get_indices([1, 5, 5, 2, 7], 8))
 # Prints []
+
+
+def can_nest(L1, L2):
+  return min(L1) > min(L2) and max(L1) < max(L2)
+
+
+print(can_nest([1, 2, 3, 4], [0, 6]))  #> True
+print(can_nest([3, 1], [4, 0]))        #> True
+print(can_nest([9, 9, 8], [8, 9]))     #> False
+print(can_nest([1, 2, 3, 4], [2, 3]))  #> False
+
+
+GUEST_LIST = {
+  "Kurt": "Germany",
+  "Julia": "France",
+  "Ito": "Japan",
+  "Katherine": "England",
+  "Sam": "Argentina"
+}
+
+# Write your function, here.
+
+
+def greeting(name):
+  if name in GUEST_LIST:
+    return f'Hi! I\'m {name} from {GUEST_LIST[name]}.'
+  else:
+    return "Hi! I'm a guest."
+
+print(greeting("Kurt"))   #> "Hi! I'm Kurt from Germany."
+print(greeting("Sam"))    #> "Hi! I'm Sam from Argentina."
+print(greeting("Monty"))  #> "Hi! I'm a guest."
