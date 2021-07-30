a6e6e2cad7f4005e75aa63e82c3d98d21b53778e
Kevin Kusky <kevinkusky@gmail.com>
2021-07-30 17:14:11 -0400

    notes on is opperator

--- week1/2tuesday/ministry_of_truth.py
+++ week1/2tuesday/ministry_of_truth.py
@@ -2,9 +2,9 @@ a = 1
 b = 1.0
 c = '1'
 
-print(a == b)
-print(a == c)
-print(b == c)
+print(a == b) # True
+print(a == c) # False
+print(b == c) # False
 
 # Note '=='
 # JavaScript strict equality '===' DOES NOT WORK with Python
@@ -15,6 +15,40 @@ print(b == c)
 #     print("a matches b")
 # else:
 #     print('not a match')
+
+
+# ***********
+# Identity
+# ***********
+
+# print(a == b) # True
+# print(a is b) # False
+
+# print(b == 1) # True
+# print(b is 1) # Evaluated to False BUT produces a warning - 'is with a literal'
+
+# print(c == '1') # True
+# print(c is '1') # Evaluates to True BUT produces a warning - 'is with a literal'
+
+
+# If type matters - isinstance
+print(b == 1 and isinstance(b, int)) # False
+print(a == 1 and isinstance(a, int)) # True
+
+
+# With TypeCasting
+print(int(b) == 1 and isinstance(b, int)) # Converts b to an int and then checks value
+
+# More TypeCasting examples
+
+print(a)
+print(float(a))
+print(str(a))
+
+# IMPORTANT
+# is looks for 'the same copy' or the same item
+print(str(a) is c) # False
+print(str(a) == c) # True
 
 # truthy values
 a = 'dart'
